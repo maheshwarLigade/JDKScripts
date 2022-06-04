ce52b71519fd7f1a63bcabdb949efe7b702b9267
maheshwarligade <maheshwar.ligade@gmail.com>
2022-06-04 12:37:08 +0530

    changes in the script for JRE

--- jdk17.bat
+++ jdk17.bat
@@ -1,4 +1,6 @@
 @echo off
 set JAVA_HOME=C:\Program Files\Java\jdk-17.0.2
+set JRE_HOME=C:\Program Files\Java\jre1.8.0_311
 set Path=%JAVA_HOME%\bin;%Path%
+set Path=%JRE_HOME%\bin;%Path%
 echo Java 17 activated.
--- jdk9.bat
+++ jdk9.bat
@@ -1,4 +1,6 @@
 @echo off
 set JAVA_HOME=C:\Program Files\Java\jdk-9.0.4
+set JRE_HOME=C:\Program Files\Java\jre-9.0.4
 set Path=%JAVA_HOME%\bin;%Path%
+set Path=%JRE_HOME%\bin;%Path%
 echo Java 9 activated.
